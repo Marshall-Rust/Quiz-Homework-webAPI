ee978ec48c146811a44c5d746782e501fa63f5ed
Marshall Rust <Marshall_Rust@yahoo.com>
2022-04-16 17:19:52 -0700

    trying to fix and change stuff. niot getting much to work

---
+++ js2.js
@@ -0,0 +1,81 @@
+
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "1. What is the final evolution of Bulbasaur?",
+      choices: ["Muk", "Venusaur", "Pidgy", "Charizard?"],
+      answer: "Venusaur"
+    },
+    {
+      title: "2. How does Clefairy evolve?",
+      choices: [
+        "Leveling",
+        "Trading",
+        "Moon Stone",
+        "None of the above" ],
+      answer: "Moon Stone"
+    },
+    {
+      title: "3. What type of Pokemon is Ninetales?",
+      choices: ["Water", "Grass", "Normal", "Fire"],
+      answer: "Fire"
+    },
+    {
+      title: "4. This pokemon has no evolution",
+      choices: [
+        "Seel",
+        "Grimer",
+        "Machoke",
+        "Farfetch'd"
+      ],
+      answer: "Farfetch'd"
+    },
+    {
+      title:
+        "5. Which was the first pokemon created by GameFreak?",
+      choices: [
+        "Pikacha",
+        "Clefairy",
+        "Rhydon",
+        "Ditto"
+      ],
+      answer: "Rhydon"
+    },
+    {
+      title: "6. What can Eevee evolve into?",
+      choices: [
+        "Vaporeon",
+        "Jolteon",
+        "Flareon",
+        "All of the above"
+      ],
+      answer: "All of the above"
+    },
+    {
+      title: "7. What Pokemon can you earn from gambling?",
+      choices: ["Abra", "Scyther", "Pinsir", "Porygon", "Dratini", "All of the above"],
+      answer: "All of the above"
+    },
+    {
+      title: "8. Who is the fastest flying Pokemon?",
+      choices: [
+        "Dragonite",
+        "Pidgeot",
+        "Butterfree",
+        "Fearow"
+      ],
+      answer: "Dragonite"
+    },
+    {
+      title: "9. Who is the best Pokemon?",
+      choices: [
+        "Diglett",
+        "Diglett",
+        "Diglett",
+        "Diglett"
+      ],
+      answer: "Diglett"
+    }
+    
+    
+  ];
